70dafc98524ed6af07ed0b6d6335caae75685dc8
Alba Gómez Pérez <albagope@gmail.com>
2019-07-29 08:44:50 +0200

    generate random number

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 'use strict'; 
+
+const inputFill = document.querySelector('.input'); 
+const button = document.querySelector('.check'); 
+const textResult = document.querySelector('.message'); 
+const numCont = document.querySelector('.cont'); 
+const randomNum = random(100);
+
+
+// function checking()
+
+
+function random(max){
+	return Math.floor(Math.random() * max);
+  }
+  console.log(randomNum); 
+  
+
+// inputFill =('click', checking); 
