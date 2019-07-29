db7e67e1b301412c9321cf15c8b3ea7bc4e66224
Alba Gómez Pérez <albagope@gmail.com>
2019-07-29 09:23:40 +0200

    resolve exercise

--- main.js
+++ main.js
@@ -5,15 +5,39 @@ const button = document.querySelector('.check');
 const textResult = document.querySelector('.message'); 
 const numCont = document.querySelector('.cont'); 
 const randomNum = random(100);
+let cont = 0; 
 
 
-// function checking()
-
-
+// función número random
 function random(max){
 	return Math.floor(Math.random() * max);
   }
   console.log(randomNum); 
   
 
-// inputFill =('click', checking); 
+function checking(){
+	cont = cont + 1; 
+	numCont.innerHTML=cont; 
+
+
+	let numberUser = inputFill.value; 
+	// numero más alto
+	if(numberUser > randomNum){
+	textResult.innerHTML = 'Lo siento, tu número es demasiado alto'; 
+}else{
+	// numero mas bajo
+	if(numberUser < randomNum){
+		textResult.innerHTML = 'Lo siento, tu número es demasiado bajo'; 
+	}else{
+		// número acertado
+		textResult.innerHTML = 'Has ganado, campeona';
+	}
+	}
+}
+
+
+
+
+
+
+button.addEventListener('click', checking); 
