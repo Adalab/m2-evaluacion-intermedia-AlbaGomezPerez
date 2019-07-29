3ccba0a6b1cbc3f3f74d5a20d9cc4ed4764aa8c5
Alba Gómez Pérez <albagope@gmail.com>
2019-07-29 10:10:46 +0200

    improve

--- main.js
+++ main.js
@@ -11,35 +11,32 @@ let cont = 0;
 // función número random
 function random(max){
 	return Math.floor(Math.random() * max);
-  }
-  console.log(randomNum); 
-  
+}
+console.log(randomNum); 
+
 
 function checking(){
 	// al contador le decimos que sume uno cada vez que hacemos click
 	cont = cont + 1; 
 	numCont.innerHTML=cont; 
-
+	
 	// hacemos una variable (va a cambiar) sobre el número que introducde el usuario
 	let numberUser = inputFill.value; 
-	
+
 	// numero más alto
 	if(numberUser > randomNum){
-	textResult.innerHTML = 'Lo siento, tu número es demasiado alto'; 
-}else{
-	// numero mas bajo
-	if(numberUser < randomNum){
-		textResult.innerHTML = 'Lo siento, tu número es demasiado bajo'; 
+		textResult.innerHTML = 'Lo siento, tu número es demasiado alto'; 
 	}else{
-		// número acertado
-		textResult.innerHTML = 'Has ganado, campeona';
-	}
+		// numero mas bajo
+		if(numberUser < randomNum){
+			textResult.innerHTML = 'Lo siento, tu número es demasiado bajo'; 
+		}else{
+			// número acertado
+			textResult.innerHTML = 'Has ganado, campeona';
+		}
 	}
 }
 
 
-
-
-
-
+// evento (click)
 button.addEventListener('click', checking); 
