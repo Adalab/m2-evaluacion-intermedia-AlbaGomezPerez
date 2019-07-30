c57bc8b3cabf5da339d9aa575d89efe8a68ee93a
Alba Gómez Pérez <albagope@gmail.com>
2019-07-30 19:25:24 +0200

    fix typo

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ const inputFill = document.querySelector('.input');
 const button = document.querySelector('.check'); 
 const textResult = document.querySelector('.message'); 
 const numCont = document.querySelector('.cont'); 
-const randomNum = random(100);
+let randomNum = random(100);
 let cont = 0; 
+const resetButton = document.querySelector('.reset'); 
 
 
 // función número random
 function random(max){
-	return Math.floor(Math.random() * max);
+	const randomNumberGenerated = Math.floor(Math.random() * max);
+	console.log(randomNumberGenerated); 
+	return randomNumberGenerated;
 }
-console.log(randomNum); 
 
 
 function checking(){
@@ -31,12 +33,34 @@ function checking(){
 		if(numberUser < randomNum){
 			textResult.innerHTML = 'Lo siento, tu número es demasiado bajo'; 
 		}else{
+			// número mayor de 100
+		// 	if(numberUser > 100){
+		// 	textResult.innerHTML = 'Recuerda, tienes que elegir un número menor de 100';
+		// }else{
+		
 			// número acertado
 			textResult.innerHTML = 'Has ganado, campeona';
-		}
+			}
 	}
 }
 
+function reset(){
+	inputFill.value = '';
+	numCont.innerHTML = '0'; 
+	cont = 0;  
+	textResult.innerHTML = 'Escribe un número y dale a <span class="cursive">Comprueba</span>'
+	randomNum = random(100);
+}
 
 // evento (click)
 button.addEventListener('click', checking); 
+
+// evento (click reset)
+resetButton.addEventListener('click', reset); 
+
+// numero mayor de 100 
+// numero menor de 0 
+// parseInt 
+// input vacio 
+// boton reset 
+// key???
