91f6543ea45e1e060f61886f40c89eb40243099a
Alba Gómez Pérez <albagope@gmail.com>
2019-07-30 19:36:21 +0200

    Restore lower number message

--- main.js
+++ main.js
@@ -21,13 +21,17 @@ function checking(){
 	// al contador le decimos que sume uno cada vez que hacemos click
 	cont = cont + 1; 
 	numCont.innerHTML=cont; 
-	
+
 	// hacemos una variable (va a cambiar) sobre el número que introducde el usuario
 	let numberUser = inputFill.value; 
-
+	
 	// numero más alto
 	if(numberUser > randomNum){
-		textResult.innerHTML = 'Lo siento, tu número es demasiado alto'; 
+	textResult.innerHTML = 'Lo siento, tu número es demasiado alto'; 
+}else{
+	// numero mas bajo
+	if(numberUser < randomNum){
+		textResult.innerHTML = 'Lo siento, tu número es demasiado bajo'; 
 	}else{
 		// numero mas bajo
 		if(numberUser < randomNum){
